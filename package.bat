6028403232436bbe81f0854e1f17efb833ddd275
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-01 14:57:59 -0500

    Update package.bat

--- package.bat
+++ package.bat
@@ -1,3 +1,16 @@
 @echo off
 
-echo Packaging android...
+echo Task: Build Android (1/4)
+lime build android
+echo Task: Package Android (2/4)
+powershell Compress-Archive bin\android\bin packages\android.zip
+
+echo Task: Build iOS (3/4)
+lime build ios
+echo Task: Package iOS (4/4)
+powershell Compress-Archive bin\ios\bin packages\android.zip
+
+echo Build finished
+echo Press any key to exit the compiler
+pause>nul
+exit
