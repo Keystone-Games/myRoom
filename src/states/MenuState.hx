67d57ccd5411c9774c98f794cfdc085634d0609d
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-01 14:50:33 -0500

    Update MenuState.hx

--- src/states/MenuState.hx
+++ src/states/MenuState.hx
@@ -5,6 +5,10 @@ class MenuState extends FlxState {
   
   public override function create() { 
     super.create(); 
+    
+    title = new FlxText(0, 0, 0, "myRoom").setFormat("monsterrat", 16, FlxColor.WHITE, CENTER);
+    title.screenCenter(X);
+    add(title);
   }
   public override function update(dt:Float) { 
     super.update(dt); 
