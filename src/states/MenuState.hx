5a3f74ac16ffe6ad9552a9a7dc66394236c817be
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-01 14:17:12 -0500

    Update MenuState.hx

--- src/states/MenuState.hx
+++ src/states/MenuState.hx
@@ -1,6 +1,12 @@
 package states;
 
 class MenuState extends FlxState {
-  public override function create() { super.create(); }
-  public override function update(dt:Float) { super.update(dt); }
+  public var title:FlxText;
+  
+  public override function create() { 
+    super.create(); 
+  }
+  public override function update(dt:Float) { 
+    super.update(dt); 
+  }
 }
