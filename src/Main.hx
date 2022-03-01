ea56a44814fe8347b7a93bf8270ac9cd528981df
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-01 13:09:34 -0500

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -1,10 +1,11 @@
 package;
 
+import flixel.FlxGame;
 import openfl.display.Sprite;
 import openfl.Lib;
 
 class Main extends Sprite {
   public function new() {
-    Lib.current.addChild();
+    Lib.current.addChild(new FlxGame(0, 0, MenuState));
   }
 }
